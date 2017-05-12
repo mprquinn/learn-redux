f3a2a27c02c87db7343a94aa5d1fa6037304c036
Mike <mprquinn@gmail.com>
2017-05-12 09:40:47 -0600

    remove comment. had to rewrite component in deprecated manner

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -4,11 +4,36 @@
 
 // 2 a copy of the current state
 
+function postComments(state = [], action) {
+	switch(action.type) {
+		case 'ADD_COMMENT':
+			return [...state, {
+				user: action.author,
+				text: action.comment
+			}];
+		case 'REMOVE_COMMENT':
+			console.log(action);
+			return [
+				...state.slice(0, action.index),
+				...state.slice(action.index + 1)
+			]
+
+		default:
+			return state;
+	}
+	return state;
+}
+
+
 function comments (state = [], action) {
 	// console.log(state, action);
-	switch (action) {
-		case 'ADD_COMMENT':
-			
+	if (typeof action.postId !== undefined) {
+		return {
+			// take the current state
+			...state,
+			// overwrite this post with a new one
+			[action.postId]: postComments(state[action.postId], action)
+		}
 	}
 	return state;
 }
